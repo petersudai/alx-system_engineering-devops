40b8bfc6fe59c2dddc6ff28a6cdcbd24c750719a
petersudai <psudai@gmail.com>
2024-01-30 23:52:52 -0800

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
-log_File = ARGV[0]
+log_file = ARGV[0]
 
 File.open(log_file, 'r').each_line do |line|
-  sender = line.match(/\[from:([^[\]]+)\]/)&.captures&.first
-  receiver = line.match(/\[to:([^[\]]+)\]/)&.captures&.first
-  flags = line.match(/\[flags:([^[\]]+)\]/)&.captures&.first
+  sender = line.match(/\[from:(\+?\w+)\]/)&.captures&.first
+  receiver = line.match(/\[to:(\+?\w+)\]/)&.captures&.first
+  flags = line.match(/\[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/)&.captures&.first
 
   puts "#{sender},#{receiver},#{flags}" if sender && receiver && flags
 end
