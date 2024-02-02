3ff60f5bbb5f8183e846cc962f21b885ebbb9922
petersudai <psudai@gmail.com>
2024-02-02 04:32:58 -0800

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
-puts File.readlines(ARGV[0]).map { |line| line.scan(/\[from:([^[\]]+)\] \[to:([^[\]]+)\] \[flags:([^[\]]+)\]/).map { |sender, receiver, flags| "#{sender},#{receiver},#{flags}" } }.join("\n")
+puts ARGV[0].scan(/\[from:([^[\]]+)\] \[to:([^[\]]+)\] \[flags:([^[\]]+)\]/).map { |sender, receiver, flags| "#{sender},#{receiver},#{flags}" } }.join(",")
 
