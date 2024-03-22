924ab49b7fed65de3825fba23a2d079d38b9d01b
petersudai <psudai@gmail.com>
2024-03-22 02:48:58 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Define file resource to create /tmp/school
 file { '/tmp/school/':
-  ensure => present,
+  ensure  => present,
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
