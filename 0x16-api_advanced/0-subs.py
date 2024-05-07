89dc61ea2621f690aac79555f3a10f692bbacd47
petersudai <psudai@gmail.com>
2024-05-07 11:56:10 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns number of subscribers for given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -18,11 +18,3 @@ def number_of_subscribers(subreddit):
         return data['data']['subscribers']
     else:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
