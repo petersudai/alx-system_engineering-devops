9dcb677ee9203ebe7e9f8659f3ab3d605f8d969d
petersudai <psudai@gmail.com>
2024-05-07 11:07:45 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns number  of subsrcibers for given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns number of subscribers for given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+
+        return 0
