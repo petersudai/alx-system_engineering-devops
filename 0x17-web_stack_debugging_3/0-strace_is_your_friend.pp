3fd5caa8a64dc1236af557818d8ce2084b183d7f
petersudai <psudai@gmail.com>
2024-05-08 05:13:27 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Debug Server, fix the Apache 500 error
 exec { 'fix-wordpress-typo':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin',
+  path    => '/usr/local/bin/:/bin',
 }
