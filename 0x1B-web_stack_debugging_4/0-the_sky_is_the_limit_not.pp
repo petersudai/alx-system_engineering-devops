6e538472546da42455086f48a98a94de77414625
petersudai <psudai@gmail.com>
2024-05-16 13:28:51 -0700

    Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin'],
+  onlyif  => 'grep -q "ULIMIT=15" /etc/default/nginx',
+  notify  => Exec['nginx-restart'],
+}
+
+# Restart Nginx to apply changes
+exec { 'nginx-restart':
+  command => 'service nginx restart',
+  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  refreshonly => true,
+}
